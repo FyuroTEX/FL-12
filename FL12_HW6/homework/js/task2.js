b74781e36f54206593f4fba5074546c40d2e86ce
Yurii Levchenko <36551537+FyuroTEX@users.noreply.github.com>
2019-12-23 17:38:38 +0200

    Add files via upload

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,16 +1,15 @@
-let a, b, c, result, x1, x2, minus = -1, two = 2, four = 4;
-a = +prompt('A');
-b = +prompt('B');
-c = +prompt('C');
-result = b * b - four * a * c;
-if ( isNaN(a) || isNaN(b) || isNaN(c) ) {
-    console.log('Invalid input data');
-} else if (result === 0) {
-    console.log(`x = ${result}`);
-} else if (result > 0) {
-    x1 = (minus * b + Math.sqrt(result)) / (a * two);
-    x2 = (minus * b - Math.sqrt(result)) / (a * two);
-    console.log(`x1 = ${x1}, x2 = ${x2}`);
+let a, b, c, s, p, d = 2;
+a = +prompt('A', '');
+b = +prompt('B', '');
+c = +prompt('C', '');
+p = (a + b + c) / d;
+s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+if (isNaN(s) || a <= 0 || b <= 0 || c <= 0 || s === 0) {
+  console.log('Triangle doesn’t exist');
+} else if (a === b && b === c && c === a) {
+  console.log('Eequivalent triangle');
+} else if (a === b || b === c || c === a) {
+  console.log('Isosceles triangle');
 } else {
-    console.log(`No solution`);
+  console.log('Scalene triangle');
 }
